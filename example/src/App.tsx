96a75f3a40ab12748eb8275f4956d9fc115d227c
Tu Dat Nguyen <tudat@affinitybridge.com>
2021-02-07 12:31:54 -0800

    Add examples

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -1,32 +1,49 @@
 import * as React from 'react';
 
-import { StyleSheet, View, Text } from 'react-native';
+import { StyleSheet, Image, SafeAreaView } from 'react-native';
 import ImageLayouts from 'react-native-image-layouts';
 
+const layoutPattern = [1, 3, 2, 2, 1];
+
+const images = [
+  'https://images.pexels.com/photos/3326113/pexels-photo-3326113.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  'https://images.pexels.com/photos/3831823/pexels-photo-3831823.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  'https://images.pexels.com/photos/704971/pexels-photo-704971.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  'https://images.pexels.com/photos/2113556/pexels-photo-2113556.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  'https://images.pexels.com/photos/3850660/pexels-photo-3850660.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  'https://images.pexels.com/photos/3850925/pexels-photo-3850925.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  'https://images.pexels.com/photos/3850597/pexels-photo-3850597.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  'https://images.pexels.com/photos/3622474/pexels-photo-3622474.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  'https://images.pexels.com/photos/2113556/pexels-photo-2113556.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  'https://images.pexels.com/photos/3850660/pexels-photo-3850660.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  'https://images.pexels.com/photos/3850925/pexels-photo-3850925.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+  'https://images.pexels.com/photos/3850597/pexels-photo-3850597.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500',
+];
 
 export default function App() {
-  const [result, setResult] = React.useState<number | undefined>();
-
-  React.useEffect(() => {
-    ImageLayouts.multiply(3, 7).then(setResult);
-  }, []);
+  function renderItem(uri: string, _index: number) {
+    return <Image source={{ uri: uri }} style={styles.image} />;
+  }
 
   return (
-    <View style={styles.container}>
-      <Text>Result: {result}</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ImageLayouts
+        data={images}
+        numberOfColumns={2}
+        patterns={layoutPattern}
+        renderItem={renderItem}
+        dividerPadding={2}
+      />
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
   },
-  box: {
-    width: 60,
-    height: 60,
-    marginVertical: 20,
+  image: {
+    width: '100%',
+    height: 150,
   },
 });
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,121 @@
-export default {
-  multiply(a: number, b: number) {
-    return Promise.resolve(a * b);
-  },
-};
+import React, { useState, useEffect } from 'react';
+import { FlatList, View, FlatListProps, ViewStyle } from 'react-native';
+import styles from './styles';
+
+interface Props extends Omit<FlatListProps<any>, 'renderItem'> {
+  numberOfColumns: number;
+  patterns: number[];
+  data: any[];
+  renderItem: (item: any, index: number) => JSX.Element | JSX.Element[];
+  dividerPadding?: number; // to insert a padding for a row having multiple blocks
+  layoutStyle?: ViewStyle;
+  emptyListResultsText?: string;
+}
+
+/**
+ * Render layout for any renderable objects
+ *
+ * @param {number}  numberOfColumns default number of columns
+ * @param {number[]}  patterns arrays of columns for continuous rows
+ * @param {any[]}  data arrays of objects to fill the layout
+ * @param {number}  dividerPadding optional, padding among layout elements, default to 0
+ * @param {function}  renderItem render function (item, index) for each data object
+ * @param {}  layoutStyle optional additional style for layout content, useful when you want to have a horizonal margin
+ */
+function ImageLayout(props: Props, ref: any) {
+  const [tiles, setTiles] = useState<any[][]>([]);
+
+  function renderItem({ item, index }: { item: any[]; index: number }) {
+    const itemLength = item.length;
+    const padding = props.dividerPadding ? props.dividerPadding : 0;
+
+    return (
+      <View style={[styles.container, props.layoutStyle]}>
+        {item.map((object, innerIndex) => (
+          <View
+            key={object ? object.id || innerIndex : innerIndex}
+            style={[
+              styles.subContainer,
+              {
+                marginTop: index === 0 ? 0 : padding,
+                marginBottom: index === tiles.length - 1 ? 0 : padding,
+                marginRight: innerIndex === itemLength - 1 ? 0 : padding,
+                marginLeft: innerIndex === 0 ? 0 : padding,
+              },
+            ]}
+          >
+            {object ? props.renderItem(object, index) : null}
+          </View>
+        ))}
+      </View>
+    );
+  }
+
+  function generateKey(item: any[], index: number) {
+    // TODO: get nested object ID
+    const validObjects = item.filter((item) => item !== null);
+    return validObjects.map((object) => object.id || index).join('-');
+  }
+
+  useEffect(() => {
+    function generateLayoutFromData() {
+      const dataLength = props.data.length;
+      const patternsLength = props.patterns.length;
+      const tiles: any[][] = [];
+      let currentDataIndex = 0;
+      if (props.data.length === 0) return tiles;
+      for (let i = 0; i < patternsLength; i++) {
+        const columns = props.patterns[i];
+        if (currentDataIndex + columns < dataLength) {
+          const newObject = props.data.slice(
+            currentDataIndex,
+            currentDataIndex + columns
+          );
+          tiles.push(newObject);
+          currentDataIndex = currentDataIndex + columns;
+        } else {
+          const newObject = props.data.slice(currentDataIndex);
+          tiles.push(newObject);
+          currentDataIndex = currentDataIndex + columns;
+          break;
+        }
+      }
+      // load default pattern
+      while (currentDataIndex < dataLength) {
+        if (currentDataIndex + props.numberOfColumns < dataLength) {
+          const newObject = props.data.slice(
+            currentDataIndex,
+            currentDataIndex + props.numberOfColumns
+          );
+          tiles.push(newObject);
+        } else {
+          const newObject = props.data.slice(currentDataIndex);
+          const emptyObjectsLength = props.numberOfColumns - newObject.length;
+          for (let j = 0; j < emptyObjectsLength; j++) {
+            newObject.push(null);
+          }
+          tiles.push(newObject);
+        }
+        currentDataIndex = currentDataIndex + props.numberOfColumns;
+      }
+      return tiles;
+    }
+    const tiles = generateLayoutFromData();
+
+    setTiles(tiles);
+  }, [props.data, props.numberOfColumns, props.patterns]);
+
+  if (props.data.length === 0) return null;
+
+  return (
+    <FlatList
+      {...props}
+      ref={ref}
+      data={tiles}
+      renderItem={renderItem}
+      keyExtractor={generateKey}
+    />
+  );
+}
+
+export default React.memo(React.forwardRef(ImageLayout));
